33d18feb2e1b7c64e35241095315627883a8dbc6
LiamHz <liam.hinzman@gmail.com>
2020-02-08 12:17:54 -0500

    Initial Commit

---
+++ GLFW OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLFW OpenGL
+//
+//  Created by Liam Hinzman on 2020-02-08.
+//  Copyright © 2020 Liam Hinzman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
