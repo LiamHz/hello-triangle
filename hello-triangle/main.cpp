52165a5f1918527a428ccfdcd65394856b3c5ddc
LiamHz <liam.hinzman@gmail.com>
2020-02-10 21:51:30 -0500

    Implemented camera class, movement, and user input

--- hello-triangle/main.cpp
+++ hello-triangle/main.cpp
@@ -11,8 +11,24 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "shader.h"
+#include "camera.h"
 
 const GLint WIDTH = 800, HEIGHT = 600;
+
+// Functions
+void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f,  3.0f));
+bool firstMouse = true;
+float lastX = WIDTH / 2;
+float lastY = HEIGHT / 2;
+
+// Timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+float currentFrame;
 
 int main() {
     glfwInit();
@@ -38,6 +54,8 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    
     glewExperimental = GL_TRUE;
 
     if(GLEW_OK != glewInit()) {
@@ -160,9 +178,9 @@ int main() {
 
     // Load texture
     stbi_set_flip_vertically_on_load(true);
-    data = stbi_load("navid.jpeg", &width, &height, &nrChannels, 0);
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
     if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     } else {
         std::cout << "Failed to load texture";
@@ -184,29 +202,40 @@ int main() {
 
     // Enable z-buffer
     glEnable(GL_DEPTH_TEST);
-
+    
+    // Enable mouse input
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
     // Render loop
     while (!glfwWindowShouldClose(window)) {
+        // Per-fram time logic
+        currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        
+        processInput(window);
+        
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Bind textures
-//        glActiveTexture(GL_TEXTURE0);
-//        glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
         // Activate shader
         shader.use();
 
-        // Define transformations
-        glm::mat4 view  = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        // Pass projection matrix to shader
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+        shader.setMat4("projection", projection);
 
-        // Aassign model and view shader uniforms
+        // Camera/view transformation
+        glm::mat4 view = camera.GetViewMatrix();
         shader.setMat4("view", view);
 
-        // Render container
+        // Render boxes
         glBindVertexArray(VAO);
         for (unsigned int i = 0; i < 10; i++) {
             // Set world coordinates of object
@@ -230,4 +259,36 @@ int main() {
 
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
+    // Prevent camera jumping when mouse first enters screen
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    
+    // yoffset is reversed since y-coords go from bottom to top
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
 }
